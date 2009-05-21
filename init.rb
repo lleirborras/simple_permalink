4379b5ca160deb78bf9966f09f403d461113af63
lleirborras <memtest@gmail.com>
2009-05-21 06:32:14 -0700

    To pass the validate_presence_of :permalink on unit test I prefere to make the permalink before_validation. Also if I generate a permalink for a row I'll never change it, because it's used to link data.

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require "core_ext"
 class << ActiveRecord::Base
   def permalink( attr )
     class_eval <<-EOF, __FILE__, __LINE__
-      before_save { |r| r.permalink = r.#{attr}.to_url }
+      before_validation { |r| r.permalink = r.#{attr}.to_url if !r.#{attr}.nil? && r.permalink.nil? }
     EOF
   end
 end
