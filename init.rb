46df21a0597531c7ae1da305f30a1bea6cfce1b9
lleirborras <memtest@gmail.com>
2009-10-27 09:33:43 -0700

    Set permalink attribute as unique, and look for duplicated permalinks before validation. In case of a duplicated permalink, adds "_N" where "N" represents the total count of duplicated permalinks

--- init.rb
+++ init.rb
@@ -3,9 +3,14 @@ require "core_ext"
 class << ActiveRecord::Base
   def permalink( attr )
     class_eval <<-EOF, __FILE__, __LINE__
-      before_validation { |r| r.permalink = r.#{attr}.to_url if !r.#{attr}.nil? && r.permalink.nil? }
+      before_validation do |r|
+        if !r.#{attr}.nil? && r.permalink.nil?
+          total = count(:conditions => {:permalink => r.#{attr}.to_url}) + 1
+          r.permalink = r.#{attr}.to_url + (total.zero? ? "" : "-" + total.to_s)  
+        end
+      end
+      validates_presence_of :permalink
+      validates_uniqueness_of :permalink
     EOF
   end
 end
-
-
